87b6ee6fdba7aee1a2c88b7c4902eef4b131a0c8
杜科 <duke@zhihu.com>
2021-10-13 11:47:55 +0800

    init

--- app/src/main/java/com/duke/dkskin/activities/MainActivity.java
+++ app/src/main/java/com/duke/dkskin/activities/MainActivity.java
@@ -7,12 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.duke.dkskin.R;
-import com.duke.dkskin.common.DynamicPermissionCompat;
+import com.duke.dkskin.common.DPermission;
 import com.duke.dkskin.common.FileUtils;
 import com.duke.dkskin.common.MyAdapter;
 import com.duke.skinlib.skin.base.BaseSkinActivity;
@@ -27,7 +26,6 @@ public class MainActivity extends BaseSkinActivity implements View.OnClickListen
     private RecyclerView recyclerview;
     private MyAdapter adapter;
 
-    private DynamicPermissionCompat dynamicPermissionCompat;
     private String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
     private int requestCode = 1000;
 
@@ -43,23 +41,26 @@ public class MainActivity extends BaseSkinActivity implements View.OnClickListen
         recyclerview.setLayoutManager(new LinearLayoutManager(this));
         recyclerview.setAdapter(adapter = new MyAdapter(this, list));
         adapter.setClickCallback(this);
-        dynamicPermissionCompat = DynamicPermissionCompat.getInstanceIfNull(this, dynamicPermissionCompat);
 
     }
 
     private void copyApk() {
         String name = "skinResApp-debug.skin";
-        File file = new File("/sdcard/" + name);
+        File file = new File(getExternalFilesDir(null) + File.separator + name);
         FileUtils.copyAssetsFileTo(this, name, file);
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn) {
-            dynamicPermissionCompat.setOnPermissionListener(permissionListener)
-                    .setPermissions(permissions)
-                    .setRequestCode(requestCode)
-                    .start();
+            DPermission.newInstance(this)
+                    .setCallback(new DPermission.DCallback() {
+
+                        @Override
+                        public void onResult(ArrayList<DPermission.PermissionInfo> permissionInfoList) {
+                            startActivity(new Intent(MainActivity.this, SkinSettingsActivity.class));
+                        }
+                    }).startRequest(permissions);
         }
     }
 
@@ -75,29 +76,4 @@ public class MainActivity extends BaseSkinActivity implements View.OnClickListen
         Toast.makeText(this, value == null ? "null" : String.valueOf(value), Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (dynamicPermissionCompat != null) {
-            dynamicPermissionCompat.onRequestPermissionsResult(permissions, grantResults);
-        }
-    }
-
-    private DynamicPermissionCompat.OnPermissionListener permissionListener = new DynamicPermissionCompat.OnPermissionListener() {
-        @Override
-        public void success(int requestCode) {
-            startActivity(new Intent(MainActivity.this, SkinSettingsActivity.class));
-            //finish();
-        }
-
-        @Override
-        public void failAndTipUser(int requestCode, List<String> deniedPermissions) {
-            Toast.makeText(MainActivity.this, "用户拒绝了", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void alwaysDenied(int requestCode, List<String> deniedPermissions) {
-            Toast.makeText(MainActivity.this, "用户勾选了\"不再提醒\"", Toast.LENGTH_SHORT).show();
-        }
-    };
 }
